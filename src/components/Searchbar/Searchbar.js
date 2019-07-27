c87cfdda0ca7dd975eada513347877ceb2223f91
Patryk Stanek <p.stanek3@gmail.com>
2019-07-27 22:25:21 +0200

    searchbar base finished

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,24 +1,64 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 import { searchProducts, getProducts } from '../../modules/Product/Product.actions';
 
 class Searchbar extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            fireRedirect: false,
+            searchedProduct: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
-        
+        this.props.dispatch(getProducts());
+        // console.log(this.props);
     }
 
-    search(event) {
-        this.props.dispatch(searchProducts(event.target.value));
+    cleanInput() {
+        document.getElementById('Form').reset();
+    }
+
+    handleChange(event) {
+        this.setState({searchedProduct: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.dispatch(searchProducts(this.state.searchedProduct));
+        console.log(this.props.visibleProducts);
+        this.setState({fireRedirect: true});
+        this.cleanInput();
+
     }
 
     render() {
+        const { from } = '/';
+        const { fireRedirect } = this.state;
+
         return (
-            <input type='text' onChange={this.search.bind(this)} />
+            <div>
+                <form id='Form' onSubmit={this.handleSubmit}>
+                    <input type='text' placeholder='Search products...' onChange={this.handleChange} />
+                </form>
+                {
+                    fireRedirect && (
+                        <Redirect to={from || `/products/product/${this.props.visibleProducts[0].id}`}/>
+                    )
+                }
+            </div>
         )
     }
 }
 
-export default Searchbar;
+const mapStateToProps = store => ({
+    products: store.productsReducer.products,
+    visibleProducts: store.productsReducer.visibleProducts
+});
+
+export default connect(mapStateToProps)(Searchbar);
--- src/store.js
+++ src/store.js
@@ -3,4 +3,6 @@ import reducers from './reducers';
 
 const store = createStore(reducers);
 
+store.subscribe(() => console.log(store.getState()));
+store.subscribe(() => console.log('changes'));
 export default store;
