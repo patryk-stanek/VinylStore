fd31516928d36f5a184346d8e74aeb48075a91d4
Patryk Stanek <p.stanek3@gmail.com>
2019-08-07 17:11:23 +0200

    Homepage styling expanded

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -1,10 +1,67 @@
 //Importing methods
 import React from "react";
 
+//Importing styles
+import "./FAQ.scss";
+
 //Creating FAQ container
 const FAQ = props => (
-    <div>
-        <h1>FAQ</h1>
+    <div className="faq">
+        <h2 className="faq__header">FAQ</h2>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #1</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #2</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #3</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #4</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #5</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #6</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #7</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #8</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
+        <div className="faq__box">
+            <h2 className="faq__question">Question #9</h2>
+            <p className="faq__answer">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+            </p>
+        </div>
     </div>
 )
 
--- src/pages/Rules/Rules.js
+++ src/pages/Rules/Rules.js
@@ -1,10 +1,55 @@
 //Importing methods
 import React from "react";
 
+//Importing styles
+import "./Rules.scss"
+
 //Creating Rules container
 const Rules = props => (
-    <div>
-        <h1>Rules</h1>
+    <div className="rules">
+        <h2 className="rules__header">Rules</h2>
+        <ul className="rules__list">
+            <li className="rules__item">
+                <p className="rules__text">
+                Lorem ipsum dolor sit amet consectetur adipiscing elit hac fames vestibulum, hendrerit tincidunt dapibus est velit aliquam nisl ac praesent, at sociis cubilia posuere neque justo ante nullam arcu.
+                </p>
+            </li>
+            <li className="rules__item">
+                <p className="rules__text">
+                Et nisl lacinia senectus suscipit aenean semper, maecenas accumsan dignissim viverra dis.
+                </p>
+            </li>
+            <li className="rules__item">
+                <p className="rules__text">
+                Enim elementum ante pretium quam metus aliquet porta, per aptent nisl egestas parturient tempus primis, eget ultrices libero lacus sem fames.
+                </p>
+            </li>
+            <li className="rules__item">
+                <p className="rules__text">
+                Elit porttitor integer aliquam scelerisque litora tortor ante massa diam accumsan fusce vitae sapien ipsum pulvinar, sem feugiat nullam eros potenti torquent a lacinia blandit leo eleifend lorem nisi.
+                </p>
+            </li>
+            <li className="rules__item">
+                <p className="rules__text">
+                Condimentum in dictum nisi lacus imperdiet nullam a sodales habitant lobortis, dis sit lacinia interdum turpis aenean ridiculus odio tincidunt fringilla, justo faucibus nam ornare amet luctus ante hac fusce.
+                </p>
+            </li>
+            <li className="rules__item">
+                <p className="rules__text">
+                Sodales egestas proin dictum eget curae, molestie rutrum cubilia.
+                </p>
+            </li>
+            <li className="rules__item">
+                <p className="rules__text">
+                Viverra porttitor et quam fames orci dictum inceptos lorem, eleifend augue diam in ultrices mi.
+                </p>
+            </li>
+            <li className="rules__item">
+                <p className="rules__text">
+                Feugiat vel vitae interdum eros egestas habitant torquent orci semper bibendum nam, sed dui suscipit taciti nascetur cras ante condimentum elit viverra.
+                </p>
+            </li>
+        </ul>
     </div>
 )
 
