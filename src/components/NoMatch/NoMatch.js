7afad31e86000ae5a3602baf8cc1beeaec059842
Patryk Stanek <p.stanek3@gmail.com>
2019-08-26 16:03:23 +0200

    NoMatch page styling

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,9 +1,14 @@
 //Importing methods
 import React from "react";
 
+//Importing styles
+import "./NoMatch.scss";
+
 //NoMatch component
 export const NoMatch = props => {
     return (
-        <h1>404!</h1>
+        <div className="no-match">
+            <h1>Page not found!</h1>
+        </div>
     )
 }
